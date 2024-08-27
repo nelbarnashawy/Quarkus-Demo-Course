6701abd6445b6cde3cc5a8228e3b0808ba89a654
Nader ElBarnashawy <nelbarnashawy@gmail.com>
2024-08-27 17:18:14 +0300

    Added Successful Unit Tests and Logging capabilities

--- src/main/java/com/sumerge/controller/MovieController.java
+++ src/main/java/com/sumerge/controller/MovieController.java
@@ -17,10 +17,13 @@ import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.jboss.logging.Logger;
 
 @Path("/movies")
 @Tag(name = "Movie Controller", description = "CRUD operations for movies")
 public class MovieController {
+
+    private Logger LOGGER = Logger.getLogger(MovieController.class);
 
     @Inject
     @ConfigProperty(name = "config.message.inject", defaultValue = "Hello from controller! No message found in config")
@@ -37,6 +40,7 @@ public class MovieController {
     @Path("/configInject")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOGGER.info("Get a hello from config injection");
         return helloMessage;
     }
 
@@ -44,6 +48,7 @@ public class MovieController {
     @Path("/configProvider")
     @Produces(MediaType.APPLICATION_JSON)
     public String getMessageByConfigProviderOptional(){
+        LOGGER.info("Get a hello from config provider");
         Config config = ConfigProvider.getConfig();
         String message = config.getOptionalValue("config.message.provider", String.class)
                 .orElse("Hello from controller! No message found in config");
@@ -58,6 +63,8 @@ public class MovieController {
             content = @Content(mediaType = MediaType.APPLICATION_JSON)
     )
     public Response getMovies() {
+        LOGGER.debug("Should Not Appear! - Get all movies from the database");
+        LOGGER.info("Get all movies from the database");
         return Response.ok(movieRepository.getMovies()).build();
     }
 
@@ -74,6 +81,7 @@ public class MovieController {
             description = "Movies size retrieved successfully", content = @Content(mediaType = MediaType.TEXT_PLAIN)
     )
     public int getMoviesSize() {
+        LOGGER.info("Get the number of movies in the database");
         return movieRepository.getMoviesSize();
     }
 
@@ -93,6 +101,7 @@ public class MovieController {
             required = true,
             content = @Content(schema = @Schema(implementation = Movie.class))
     ) Movie movie) {
+        LOGGER.info("Create a movie: " + movie.getName() + ", and saved it in the database");
         movieRepository.createMovie(movie);
         return Response.status(Response.Status.CREATED).entity(movieRepository.getMovies()).build();
     }
@@ -121,12 +130,15 @@ public class MovieController {
     public Response updateMovie(
             @Parameter(description = "Updated movie", required = true) @PathParam("updatedTitle") String updatedTitle,
             @Parameter(description = "ID of movie to be updated", required = true) @PathParam("id") Long id) {
+        LOGGER.info("Update a movie with id: " + id + " to title: " + updatedTitle);
         Movie movieToUpdate = movieRepository.getMovieById(id);
         if (movieToUpdate != null) {
+            LOGGER.info("Movie with id: " + id + " found and updated");
             movieToUpdate.setName(updatedTitle);
             movieRepository.updateMovie(movieToUpdate);
             return Response.ok(movieRepository.getMovies()).build();
         }
+        LOGGER.error("Movie with id: " + id + " not found");
         return Response.status(Response.Status.NOT_FOUND).build();
     }
 
@@ -153,10 +165,13 @@ public class MovieController {
             }
     )
     public Response deleteMovie(@PathParam("id") Long id) {
+        LOGGER.info("Delete a movie with id: " + id + " from the database");
         if (movieRepository.getMovieById(id) != null) {
+            LOGGER.info("Movie with id: " + id + " found and deleted");
             movieRepository.deleteMovieById(id);
             return Response.ok(movieRepository.getMovies()).build();
         }
+        LOGGER.error("Movie with id: " + id + " not found");
         return Response.status(Response.Status.NOT_FOUND).build();
     }
 
